13b109da644b1b553fb71fb19757403527055fcd
Anastasia Kekelidou <kekelidou@gmail.com>
2020-09-15 12:39:57 +0200

    🤏🏻

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
 app.use(express.static("public"));
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.get("/images", (req, res) => {
     //console.log("something in the get");
@@ -16,6 +38,22 @@ app.get("/images", (req, res) => {
         .catch((err) => {
             console.log("err", err);
         });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("file", req.file);
+    console.log("input", req.body);
+
+    if (req.file) {
+        ///db insert for all info
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("server is listenning..."));
--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,12 @@
             heading: "vue heading",
             // cuteAnimals: [],
             images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
         },
+
         mounted: function () {
             var that = this;
             axios
@@ -21,6 +26,32 @@
                 .catch(function (err) {
                     console.log("err in get/images:", err);
                 });
+        },
+        methods: {
+            handleClick: function (e) {
+                e.preventDefault();
+                // console.log("this: ", this);
+                var formData = new FormData();
+
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+
+                axios
+                    .post("/upload", formData)
+                    .then(function (resp) {
+                        console.log("resp in post/upload", resp);
+                    })
+                    .catch(function (err) {
+                        console.log("err in post'upload", err);
+                    });
+            },
+            handleChange: function (e) {
+                //console.log("handleChange is firering");
+                // console.log("file", e.target.files[0]);
+                this.file = e.target.files[0];
+            },
         },
     });
 })();
